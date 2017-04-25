468653710e9b6d11447a4c9962a741d586f7dedf
Kelvin Chan <chan.kelv@gmail.com>
2017-04-25 16:13:55 -0700

    Fixed storyboard. Removed all seques from main

---
+++ BCHA-Formulary/BCHA-Formulary/LaunchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchViewController.swift
+//  BCHA-Formulary
+//
+//  Created by Kelvin Chan on 2017-04-25.
+//  Copyright © 2017 BCHA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LaunchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        performSegue(withIdentifier: "MainNavControllerSeque", sender: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
