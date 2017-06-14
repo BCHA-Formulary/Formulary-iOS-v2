0ff91341a62474e45ead2f54fe74f17b4a318b01
Kelvin Chan <chan.kelv@gmail.com>
2017-06-14 13:36:15 -0700

    Forgot to finish no drug result view. Added it as before. Removed full formulary view button for now pending logic on display.

--- BCHA-Formulary/BCHA-Formulary/NoDrugFoundViewController.swift
+++ BCHA-Formulary/BCHA-Formulary/NoDrugFoundViewController.swift
@@ -12,4 +12,15 @@ import UIKit
 class NoDrugFoundViewController:UIViewController{
     var drugSearched:String!
     
+    @IBOutlet weak var drugNameTitle: UILabel!
+    @IBOutlet weak var viewFormularyBttn: UIButton!
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        title = "DRUG NOT FOUND"
+        drugNameTitle.text = "Sorry, " + drugSearched + " was not found."
+        
+        //TODO figure out what to do with button. Hide for now
+        viewFormularyBttn.isHidden = true
+    }
 }
